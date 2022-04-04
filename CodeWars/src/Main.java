1437f7a125e48b2049a05ad70025aacf13c52b2c
Chuck Feeney <chafeeney24@gmail.com>
2022-04-04 17:28:17 -0400

    No major changes

---
+++ CodeWars/src/Main.java
@@ -0,0 +1,14 @@
+import CodeWarsSandBox.Factorial;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        Factorial factorial = new Factorial();
+        System.out.println(factorial.factorial(5));
+
+    }
+
+    }
+;
